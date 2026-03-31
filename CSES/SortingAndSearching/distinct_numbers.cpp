ba3a02d8ba50f6c1c5e8d30c86ee5f8b9d73c0a2
afrozAnsaari <afrozansaari787@gmail.com>
2026-03-31 22:48:24 +0530

    Some minor changes

---
+++ CSES/SortingAndSearching/distinct_numbers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t;
+    cin >> t;
+
+    return 0;
+}
